a69b046304815a3bb38b2020189e964435da8133
hW-personal <hjwright2040@gmail.com>
2023-03-08 10:43:44 +0000

    Update Card.cs

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -14,9 +14,49 @@ namespace CMP1903M_A01_2223
         //The 'set' methods for these properties could have some validation
         //There are 52 cards in a deck
 
-        public int Value { get; set; }
-        public int Suit { get; set; }
+        //public int Value { get; set; }
+        //public int Suit { get; set; }
+
+        private int value;
+        private int suit;
+
+        Card card = new Card();
+        card.Value = 1; //minimum value
+        card.Suit = 1; //minmum suit
+        Console.WriteLine($"Value: {card.Value}, Suit: {card.Suit}");
+
+        card.Value = 13; //maximum value
+        card.Suit = 4; //maximum suit
+        Console.WriteLine($"Value: {card.Value}, Suit: {card.Suit}");
 
 
+
+
+        public int Value
+        {
+            get { return Value ; }
+            set
+            {
+                if (value < 1 || value > 13)
+                {
+                    throw new ArgumentException("Invalid card value.");
+                }
+                this.value = value;
+            }
+
+        }
+
+        public int Suit
+        {
+            get { return Suit ; }
+            set
+            { 
+                if (value < 1 || value > 4 )
+                {
+                    throw new ArgumentException("Invalid card suit.");
+                }
+                this.suit = value ;
+            }
+        }
     }
 }
