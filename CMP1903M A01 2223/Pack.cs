582b007e1f2c76bbfe4898f164e90fbd41d2af17
hW-personal <hjwright2040@gmail.com>
2023-03-09 01:23:35 +0000

    Working Files V2

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -8,11 +8,20 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        List<Card> pack;
 
+        static List<Card> pack;
+
+        //Initialising the card pack
         public Pack()
         {
-            //Initialise the card pack here
+            pack = new List<Card>();
+            for (int suit = 1; suit <= 4; suit++)
+            {
+                for (int value = 1; value <= 13; value++)
+                {
+                    pack.Add(new Card(value, suit));
+                }
+            }
         }
 
         public static bool shuffleCardPack(int typeOfShuffle)
@@ -22,15 +31,78 @@ namespace CMP1903M_A01_2223
             //  1. Fisher-Yates
             //  2. Riffle Shuffle
             //  3. No Shuffle
+
+            //  1. Fisher-Yates Shuffle
+            if (typeOfShuffle == 1)
+            {
+                Random random = new Random();
+                for (int i = (pack.Count - 1); i > 0; i--)
+                {
+                    int j = random.Next(i + 1);
+                    Card temp = pack[j];
+                    pack[j] = pack[i];
+                    pack[i] = temp;
+                }
+                return true;
+            }
+
+            //  2. Riffle Shuffle
+            else if (typeOfShuffle == 2)
+            {
+                List<Card> leftHalf = pack.GetRange(0, pack.Count / 2);
+                List<Card> rightHalf = pack.GetRange(pack.Count / 2, pack.Count - pack.Count / 2);
+                pack.Clear();
+                Random random = new Random();
+                while (leftHalf.Count > 0 && rightHalf.Count > 0)
+                {
+                    if (random.NextDouble() < 0.5)
+                    {
+                        pack.Add(leftHalf[0]);
+                        leftHalf.RemoveAt(0);
+                    }
+                    else
+                    {
+                        pack.Add(rightHalf[0]);
+                        rightHalf.RemoveAt(0);
+                    }
+                }
+                pack.AddRange(leftHalf);
+                pack.AddRange(rightHalf);
+                return true;
+            }
+
+            //  3. No Shuffle
+            else if (typeOfShuffle == 3)
+            {
+                return true;
+            }
+            return false;
+
         }
         public static Card deal()
         {
             //Deals one card
+            Card card = pack[0];
+            pack.RemoveAt(0);
+            return card;
 
         }
         public static List<Card> dealCard(int amount)
         {
             //Deals the number of cards specified by 'amount'
+            List<Card> cards = pack.GetRange(0, amount);
+            pack.RemoveRange(0, amount);
+            return cards;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (Card card in pack)
+            {
+                result += card.ToString() + "\n";
+            }
+            return result;
         }
     }
 }
